a78ad094cac790411b86960271c13d4cbb23e22e
Mikhail Y. Dmitrienko <dmitrienko@sl.iae.nsk.su>
2021-03-17 09:18:44 +0700

    last changes

--- decoder_low_quality/bitstream.h
+++ decoder_low_quality/bitstream.h
@@ -2,7 +2,9 @@
 
 #include <cstdint>
 #include <climits>
-
+#include <vector>
+#include <algorithm>
+#include "types.h"
 template <typename T>
 T swap_endian(T u)
 {
@@ -26,8 +28,8 @@ struct Bitstream
 {
     size_t size;
     size_t len_readed;
-    uint8_t *cur;
-    uint8_t *buf;
+    uint8_t* cur;
+    uint8_t* buf;
     uint8_t bitoffset;
 };
 
@@ -35,24 +37,59 @@ struct Bitstream
 void readFromBitsream(Bitstream& bitstream, uint8_t* dst, size_t size);
 
 
-template <typename T>
-void readFromBitsreamAndSwap(Bitstream& bitstream, T& dst, size_t size);
 
 
 template<typename T>
-inline void readFromBitsreamAndSwap(Bitstream& bitstream, T& dst, size_t size)
+T readFromBitsreamAndSwap(Bitstream& bitstream) 
 {
-    readFromBitsream(bitstream, (uint8_t*)&dst, size);
+    T dst;
+    readFromBitsream(bitstream, (uint8_t*)&dst, sizeof(T));
     dst = swap_endian(dst);
+    return dst;
 }
 
 template<typename T>
-inline void peekBitsreamAndSwap(Bitstream& bitstream, T& dst, size_t size)
+T peekBitsreamAndSwap(Bitstream& bitstream) 
 {
-    readFromBitsream(bitstream, (uint8_t*)&dst, size);
-    bitstream.len_readed -= size;
-    bitstream.cur -= size;
+    T dst;
+    readFromBitsream(bitstream, (uint8_t*)&dst, sizeof(T));
+    bitstream.len_readed -= sizeof(T);
+    bitstream.cur -= sizeof(T);
     dst = swap_endian(dst);
+    return dst;
+}
+
+template<typename T>
+T readBitsFromBitstream(Bitstream& bitstream, size_t bitsCount)
+{
+    std::vector<size_t> compliteTypeSizes{ 8,16,32 };
+    T value{ 0 };
+    if (std::find(compliteTypeSizes.begin(), compliteTypeSizes.end(), sizeof(T)) == compliteTypeSizes.end())
+    {
+        T mask{ 0 };
+        mask = ~mask;
+        T mask1 = mask >> (bitstream.bitoffset + bitsCount);
+        T mask2 = mask << (sizeof(T)*BYTE_SIZE - bitstream.bitoffset);
+        mask = ~(mask1 | mask2); // |000000...|(a)111111...|(b)000000...|  a pos is offset, b pos is bitsCount + offset
+        value = peekBitsreamAndSwap<T>(bitstream);
+        value = value & mask;
+        size_t valueShitRight = sizeof(T)*BYTE_SIZE - bitsCount - bitstream.bitoffset;
+        value = value >> valueShitRight;
+        bitstream.bitoffset += bitsCount;
+        while (bitstream.bitoffset >= 8)
+        {
+            bitstream.len_readed += 1;
+            bitstream.cur += 1;
+            bitstream.bitoffset -= 8;
+        }
+    }
+    else
+    {
+
+        value = readFromBitsreamAndSwap<T>(bitstream);
+    }
+
+    return value;
 }
 
 void bistreamSkip(Bitstream& bitstream, size_t skipLen);
